fe3dd0bddf6d7ae33eb2e9b99cf8379606855776
Kief Morris <kmorris@thoughtworks.com>
2013-07-19 16:25:51 +0100

    Set version properly

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,4 @@ maintainer_email  'chef@kief.com'
 license           'MIT'
 description       'Sets node attributes to describe monitoring checks'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-minor_ver = `git log -n 1 --pretty=format:'%at' #{File.dirname(__FILE__)} 2> /dev/null`
-version           '0.2.' + (minor_ver != '' ? minor_ver : '0')
+version           '0.0.1'
