b17a79ffae4636b77db9c1644b0ba9453f618fb6
Kief Morris <kmorris@thoughtworks.com>
2013-07-19 17:13:29 +0100

    Fix typpo

--- resources/http.rb
+++ resources/http.rb
@@ -1,6 +1,6 @@
 
 actions :add, :enable, :disable, :remove
-default _action :add
+default_action :add
 
 attribute :url,       :kind_of => String,   :name_attribute => true
 attribute :service,   :kind_of => String,   :default => 'undefined'
