d7a9415552aa5ec7ad72d0de355c9b033d607e91
Kief Morris <kmorris@thoughtworks.com>
2013-07-19 16:45:11 +0100

    Increment version

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,4 @@ maintainer_email  'chef@kief.com'
 license           'MIT'
 description       'Sets node attributes to describe monitoring checks'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           '0.0.1'
+version           '0.0.2'
