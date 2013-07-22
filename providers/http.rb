ce0dd8502d7edcab09ccd96e204b8f59453b51ff
Kief Morris <kmorris@thoughtworks.com>
2013-07-22 11:44:42 +0100

    Debugging

--- providers/http.rb
+++ providers/http.rb
@@ -4,6 +4,11 @@ action :add do
     node['healthchecks'][new_resource.service].nil? ||
     node['healthchecks'][new_resource.service]['http'].nil? ||
     node['healthchecks'][new_resource.service]['http'][new_resource.url].nil?
+
+puts "KSM: service: #{new_resource.service}"
+puts "KSM: url: #{new_resource.url}"
+puts "KSM: full attribute: #{node['healthchecks'][new_resource.service]['http'][new_resource.url]}"
+
       node['healthchecks'][new_resource.service]['http'][new_resource.url] = { 'enabled' => true }
   end
 end
