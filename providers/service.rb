584df817db4da6869907a185f568aae402d64fe7
Kief Morris <kmorris@thoughtworks.com>
2013-07-22 14:04:20 +0100

    Fix resource naming issue

--- providers/service.rb
+++ providers/service.rb
@@ -1,10 +1,10 @@
 
 action :add do
-  if service_defined?(new_resource.service) && service_description(new_resource.service) == new_resource.description
+  if service_defined?(new_resource.name) && service_description(new_resource.name) == new_resource.description
     new_resource.updated_by_last_action(false)
   else
-    Chef::Log.info "Defining service for healthcheck #{new_resource.service}"
-    node.set['healthchecks'][new_resource.service]['_info'] = {
+    Chef::Log.info "Defining service for healthcheck #{new_resource.name}"
+    node.set['healthchecks'][new_resource.name]['_info'] = {
       'description'     => new_resource.description,
       'current_version' => new_resource.current_version,
       'deploying'       => new_resource.deploying,
@@ -15,9 +15,9 @@ action :add do
 end
 
 action :remove do
-  if service_defined?(new_resource.service)
-    Chef::Log.info "Removing service definition for #{new_resource.service}"
-    node.set['healthchecks'][new_resource.service] = nil
+  if service_defined?(new_resource.name)
+    Chef::Log.info "Removing service definition for #{new_resource.name}"
+    node.set['healthchecks'][new_resource.name] = nil
     new_resource.updated_by_last_action(true)
   else
     new_resource.updated_by_last_action(false)
